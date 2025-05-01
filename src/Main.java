7cf306c4f7e14678d57c522774198850761216f0
Ivanp2003 <andrespanchichavez@gmail.com>
2025-05-01 07:56:35 -0500

    Parte 1

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,37 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Andrés Panchi");
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Desea ingresar al sistema?");
-
+        System.out.println("Desea ingresar al sistema? (si/no)");
         String respuesta = sc.nextLine();
 
-        while("si".equals(respuesta)) ;{
-            System.out.println("Ingrese su nombre");
-            String nombre = sc.nextLine();
-            System.out.println("Ingrese sus años de experiencia" );
-            int añosExperiencia = sc.nextInt();
-            sc.nextLine();
-            System.out.println("Ingrese su especialidad");
-            String especialidad = sc.nextLine();
+        if ("si".equals(respuesta)) {
+            int contador = 0;
 
+            while (contador < 3) {
+                System.out.println("Ingrese su nombre:");
+                String nombre = sc.nextLine();
+
+                System.out.println("Ingrese sus años de experiencia:");
+                int añosExperiencia = sc.nextInt();
+                sc.nextLine();
+                System.out.println("Ingrese su especialidad:");
+                String especialidad = sc.nextLine();
+
+
+                System.out.println("----- Resumen -----");
+                System.out.println("Nombre: " + nombre);
+                System.out.println("Años de experiencia: " + añosExperiencia);
+                System.out.println("Especialidad: " + especialidad+"/n");
+
+                contador++;
+            }
+        } else {
+            System.out.println("Adios.");
         }
+        sc.close();
     }
 }
